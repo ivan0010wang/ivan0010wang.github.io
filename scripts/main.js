bd9fdf3d54feb8734126e16ee8b790c63ad5aad6
Ivan Wang <ivanhaifengwang@users.noreply.github.com>
2020-02-24 13:19:12 -0700

    Ivan wrote the sudo code for potentially necessary js functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,33 @@
-// When the images on the bottom is clicked, 
+/** When one of the images on the bottom is clicked, the top
+ * picture should switch position with the clicked image. 
+ * NEED: a. onclick event
+ *       b. The image on top has class="restaurant-image-top",
+ *          the rest of the images has a class="restaurant-image-bottom"
+ *       c. change className of the images.
+ */ 
+
+
+/** All the information of the restaurants should come from the CMS
+ * NEED: a. fetch request
+ *       b. display the names in the list
+ */
+
+
+/** There should be a function to display restaurants
+ * with indoor/patio seatings based on the weather.
+ * NEED: a. find the restaurants with patio/indoor==true
+ *       b. change display to none for those !=true
+ */
+
+
+/** When a restaurant is clicked, the restaurant info
+ * should be updated.
+ * NEED: a. onclick event
+ *       b. display the perticular element in the JSON
+ */
+
+
+/** The user review should be added to the cms.
+ * NEED: a. assign user input to a object variable
+ *       b. patch request
+ */
