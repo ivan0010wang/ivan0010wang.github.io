b745956c5c8d95753d6a225a45101ec3f8afb280
Kevin Crawford <gadgecrawford@hotmail.com>
2020-02-27 14:07:20 -0700

    solved problem with picture swapping

--- scripts/main.js
+++ scripts/main.js
@@ -7,21 +7,24 @@
  *       c. change className of the images.
  */
 
-function replaceImageOnTop() {
-    let topImage = document.getElementsByClassName("review-images-top");
+function changeImage1() {
+
+    document.getElementById("id1").src = "./images/a.jpg"
 }
 
-//html code 
-// <span onclick="changeImage('contact.png')">Contact</span>
-// <span onclick="changeImage('address.png')">Address Card</span>
-// <span onclick="changeImage('clock.png')">Clock</span>
-// <img id="imgDisp" src="contact.png" />
+function changeImage2() {
+    document.getElementById("id1").src = "./images/b.jpg"
+}
 
-//for js
-//     function changeImage(imgName) {
-//         image = document.getElementById('imgDisp');
-//         image.src = imgName;
-//     }
+function changeImage3() {
+    document.getElementById("id1").src = "./images/c.jpg"
+}
+
+
+
+
+
+
 
 
 
@@ -31,60 +34,63 @@ function replaceImageOnTop() {
  *       b. display the names in the list
  */
 
-async function makeAListOfRestaurants(){
-  const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
-  const restaurantInfo = await response.json();
-  for (var i = 0; i < restaurantInfo.length; i++){
-    var newTag = document.createElement("button");
-    var textnode = document.createTextNode(restaurantInfo[i].name + ' ');
-    newTag.setAttribute('onclick', 'showRestaurantDetails(i)');
-    // The line above need to be tested.
-    // If not working, try researching .setAttribute.onclick = function...
-    newTag.appendChild(textnode);
-    var list = document.getElementById("restaurant");
-    list.insertBefore(newTag, list.childNodes[0]);
-  }
+async function makeAListOfRestaurants() {
+    const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
+    const restaurantInfo = await response.json();
+    for (var i = 0; i < restaurantInfo.length; i++) {
+        var newTag = document.createElement("button");
+        var textnode = document.createTextNode(restaurantInfo[i].name + ' ');
+        newTag.setAttribute('onclick', 'showRestaurantDetails(i)');
+        // The line above need to be tested.
+        // If not working, try researching .setAttribute.onclick = function...
+        newTag.appendChild(textnode);
+        var list = document.getElementById("restaurant");
+        list.insertBefore(newTag, list.childNodes[0]);
+    }
 }
 makeAListOfRestaurants();
 
-function giveMeFive(){
-  console.log("FIVE!");
+function giveMeFive() {
+    console.log("FIVE!");
 }
 
-function showRestaurantDetails(i){
-  showRestaurantName(i);
-  showRestaurantType(i);
-  showRestaurantDining(i);
-  showRestaurantDescription(i);
+function showRestaurantDetails(i) {
+    showRestaurantName(i);
+    showRestaurantType(i);
+    showRestaurantDining(i);
+    showRestaurantDescription(i);
 }
 
-function showRestaurantName(i){
-  var newTag = document.createElement("h1");
-  var textnode = document.createTextNode("Name: " + restaurantInfo[i].name);
-  newTag.appendChild(textnode);
-  var list = document.getElementById("info");
-  list.insertAfter(newTag, list.childNodes[0]);
+function showRestaurantName(i) {
+    var newTag = document.createElement("h1");
+    var textnode = document.createTextNode("Name: " + restaurantInfo[i].name);
+    newTag.appendChild(textnode);
+    var list = document.getElementById("info");
+    list.insertAfter(newTag, list.childNodes[0]);
 }
-function showRestaurantType(i){
-  var newTag = document.createElement("span");
-  var textnode = document.createTextNode("Cuisine " + restaurantInfo[i].type);
-  newTag.appendChild(textnode);
-  var list = document.getElementById("info");
-  list.insertAfter(newTag, list.childNodes[1]);
+
+function showRestaurantType(i) {
+    var newTag = document.createElement("span");
+    var textnode = document.createTextNode("Cuisine " + restaurantInfo[i].type);
+    newTag.appendChild(textnode);
+    var list = document.getElementById("info");
+    list.insertAfter(newTag, list.childNodes[1]);
 }
-function showRestaurantDining(i){
-  var newTag = document.createElement("span");
-  var textnode = document.createTextNode("Dining " + restaurantInfo[i].dining);
-  newTag.appendChild(textnode);
-  var list = document.getElementById("info");
-  list.insertAfter(newTag, list.childNodes[2]);
+
+function showRestaurantDining(i) {
+    var newTag = document.createElement("span");
+    var textnode = document.createTextNode("Dining " + restaurantInfo[i].dining);
+    newTag.appendChild(textnode);
+    var list = document.getElementById("info");
+    list.insertAfter(newTag, list.childNodes[2]);
 }
-function showRestaurantDescription(i){
-  var newTag = document.createElement("p");
-  var textnode = document.createTextNode("Description" + restaurantInfo[i].description);
-  newTag.appendChild(textnode);
-  var list = document.getElementById("info");
-  list.insertBefore(newTag, list.childNodes[3]);
+
+function showRestaurantDescription(i) {
+    var newTag = document.createElement("p");
+    var textnode = document.createTextNode("Description" + restaurantInfo[i].description);
+    newTag.appendChild(textnode);
+    var list = document.getElementById("info");
+    list.insertBefore(newTag, list.childNodes[3]);
 }
 // End
 
@@ -112,52 +118,53 @@ function showRestaurantDescription(i){
  *       b. innerHTML these restaurants
  */
 
-async function showRestaurantsWithPatio(){
-  const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
-  const restaurantInfo = await response.json();
-  for (var i = 0; i < restaurantInfo.length; i++){
-    if (restaurantInfo[i].patio === true){
-      var newTag = document.createElement("button");
-      var textnode = document.createTextNode(restaurantInfo[i].name + ' ')
-      newTag.appendChild(textnode);
-      var list = document.getElementById("patio");
-      list.insertBefore(newTag, list.childNodes[0]);
+async function showRestaurantsWithPatio() {
+    const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
+    const restaurantInfo = await response.json();
+    for (var i = 0; i < restaurantInfo.length; i++) {
+        if (restaurantInfo[i].patio === true) {
+            var newTag = document.createElement("button");
+            var textnode = document.createTextNode(restaurantInfo[i].name + ' ')
+            newTag.appendChild(textnode);
+            var list = document.getElementById("patio");
+            list.insertBefore(newTag, list.childNodes[0]);
+        }
     }
+    showRestaurantsWithPatio();
+    // End
+
+
+
+    /** When a restaurant is clicked, the restaurant info
+     * should be updated.
+     * 
+     * NEED: a. onclick event
+     *       b. display the perticular element
+     */
+
+
+
+
+    /** The user review should be added to the cms.
+     * NEED: a. assign user input to a object variable
+     *       b. patch request
+     */
+
+
+
+    /** A function to rank all restaurants based on average score.
+     * 
+     * Need: a. array.sort()
+     *       b. display the sorted version of the array
+     *       c. example
+     *          var employees=[]
+                employees[0]={name:"George", age:32, retiredate:"March 12, 2014"}
+                employees[1]={name:"Edward", age:17, retiredate:"June 2, 2023"}
+                employees[2]={name:"Christine", age:58, retiredate:"December 20, 2036"}
+                employees[3]={name:"Sarah", age:62, retiredate:"April 30, 2020"}
+
+                employees.sort(function(a, b){
+                    return a.age-b.age
+                })
+     */
 }
-showRestaurantsWithPatio();
-// End
-
-
-
-/** When a restaurant is clicked, the restaurant info
- * should be updated.
- * 
- * NEED: a. onclick event
- *       b. display the perticular element
- */
-
-
-
-
-/** The user review should be added to the cms.
- * NEED: a. assign user input to a object variable
- *       b. patch request
- */
-
-
-
-/** A function to rank all restaurants based on average score.
- * 
- * Need: a. array.sort()
- *       b. display the sorted version of the array
- *       c. example
- *          var employees=[]
-            employees[0]={name:"George", age:32, retiredate:"March 12, 2014"}
-            employees[1]={name:"Edward", age:17, retiredate:"June 2, 2023"}
-            employees[2]={name:"Christine", age:58, retiredate:"December 20, 2036"}
-            employees[3]={name:"Sarah", age:62, retiredate:"April 30, 2020"}
-
-            employees.sort(function(a, b){
-                return a.age-b.age
-            })
- */
