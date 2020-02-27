1a184be0dc1be9c8b3a1c49e34c7cfb8ae17c23c
Kevin Crawford <gadgecrawford@hotmail.com>
2020-02-27 09:46:45 -0700

    Working on adding pics

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,23 @@
  *       b. The image on top has class="restaurant-image-top",
  *          the rest of the images has a class="restaurant-image-bottom"
  *       c. change className of the images.
- */ 
-function replaceImageOnTop(){
-  let topImage = document.getElementsByClassName("review-images-top");
-  
+ */
+function replaceImageOnTop() {
+    let topImage = document.getElementsByClassName("review-images-top");
+
 }
+
+//html code 
+// <span onclick="changeImage('contact.png')">Contact</span>
+// <span onclick="changeImage('address.png')">Address Card</span>
+// <span onclick="changeImage('clock.png')">Clock</span>
+// <img id="imgDisp" src="contact.png" />
+
+//for js
+//     function changeImage(imgName) {
+//         image = document.getElementById('imgDisp');
+//         image.src = imgName;
+//     }
 
 
 /** The name of the restaurants should come from the CMS
@@ -17,16 +29,16 @@ function replaceImageOnTop(){
  * NEED: a. fetch request
  *       b. display the names in the list
  */
-async function makeAListOfRestaurants(){
-  const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
-  const restaurantInfo = await response.json();
-  for (var i = 0; i < restaurantInfo.length; i++){
-    var newTag = document.createElement("span");
-    var textnode = document.createTextNode(restaurantInfo[i].name + ' ')
-    newTag.appendChild(textnode);
-    var list = document.getElementById("restaurantNames");
-    list.insertBefore(newTag, list.childNodes[0]);
-  }
+async function makeAListOfRestaurants() {
+    const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
+    const restaurantInfo = await response.json();
+    for (var i = 0; i < restaurantInfo.length; i++) {
+        var newTag = document.createElement("span");
+        var textnode = document.createTextNode(restaurantInfo[i].name + ' ')
+        newTag.appendChild(textnode);
+        var list = document.getElementById("restaurantNames");
+        list.insertBefore(newTag, list.childNodes[0]);
+    }
 }
 makeAListOfRestaurants();
 // End
@@ -38,18 +50,18 @@ makeAListOfRestaurants();
  * NEED: a. find the restaurants with patio/indoor==true
  *       b. innerHTML these restaurants
  */
-async function showRestaurantsWithPatio(){
-  const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
-  const restaurantInfo = await response.json();
-  for (var i = 0; i < restaurantInfo.length; i++){
-    if (restaurantInfo[i].patio === true){
-      var newTag = document.createElement("span");
-      var textnode = document.createTextNode(restaurantInfo[i].name + ' ')
-      newTag.appendChild(textnode);
-      var list = document.getElementById("patio");
-      list.insertBefore(newTag, list.childNodes[0]);
+async function showRestaurantsWithPatio() {
+    const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
+    const restaurantInfo = await response.json();
+    for (var i = 0; i < restaurantInfo.length; i++) {
+        if (restaurantInfo[i].patio === true) {
+            var newTag = document.createElement("span");
+            var textnode = document.createTextNode(restaurantInfo[i].name + ' ')
+            newTag.appendChild(textnode);
+            var list = document.getElementById("patio");
+            list.insertBefore(newTag, list.childNodes[0]);
+        }
     }
-  }
 }
 showRestaurantsWithPatio();
 // End
