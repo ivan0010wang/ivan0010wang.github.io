8059130c74765c82339b0b5c23030ab3630332fc
Ivan Wang <ivanhaifengwang@users.noreply.github.com>
2020-02-28 00:27:04 -0700

    IT IS WORKING

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,49 @@
-/** 
- * When one of the images on the bottom is clicked, the top
- * picture should switch position with the clicked image. 
- */
-
-
-
-/** 
+/**
  * Fetch the info of the restaurants from the CMS
  */
-async function getRestaurantsInfo(){
-  const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Restaurants");
+async function getRestaurantsInfo() {
+  const response = await fetch(
+    "http://red-strapi-postgres-heroku.herokuapp.com/Restaurants"
+  );
   var restaurantInfo = await response.json();
   return restaurantInfo;
 }
 var restaurantInfo = getRestaurantsInfo();
 // End
 
+/**
+ * Put the clicked img on top
+ */
+function changeImage1() {
+  document.getElementById("id1").src =
+    "/images/joseph-gonzalez-egg-unsplash-resized.jpg";
+}
 
+function changeImage2() {
+  document.getElementById("id1").src =
+    "/images/jay-wennington-N_Y88TWmGwA-unsplash-resized.jpg";
+}
+
+function changeImage3() {
+  document.getElementById("id1").src =
+    "/images/brooke-lark-oaz0raysASk-unsplash-resized.jpg";
+}
+//End
 
 /**
  * The names of the restaurants should be buttons with dynamic id
  */
-restaurantInfo.then(
-  info => {
-    for (var i = 0; i < info.length; i++){
-      var newTag = document.createElement("button");
-      var textnode = document.createTextNode(info[i].name);
-      newTag.appendChild(textnode);
-      newTag.setAttribute('id','restaurantButton'+i)
-      var list = document.getElementById("restaurantNames");
-      list.insertBefore(newTag, list.childNodes[0]);
-    }
+restaurantInfo.then(info => {
+  for (var i = 0; i < info.length; i++) {
+    var newTag = document.createElement("button");
+    var textnode = document.createTextNode(info[i].name);
+    newTag.appendChild(textnode);
+    newTag.setAttribute("id", "restaurantButton" + i);
+    var list = document.getElementById("restaurantNames");
+    list.insertBefore(newTag, list.childNodes[0]);
   }
-)
+});
 // End
-
-
 
 /**
  * A function to avoid repeating code when creating new nodes
@@ -45,78 +53,60 @@ restaurantInfo.then(
  * @param {*} targetID is the id in html where the node will be placed
  * @param {*} where is where the node will be placed in the targetID
  */
-function writeOnWebPage(newElement,text1,text2,targetID,where){
+function writeOnWebPage(newElement, text1, text2, targetID, where) {
   var newTag = document.createElement(newElement);
-  var textNode = document.createTextNode(text1+text2);
+  var textNode = document.createTextNode(text1 + text2);
   newTag.appendChild(textNode);
   var list = document.getElementById(targetID);
-  // Here we need to somehow delete whatever is already there in the div
   list.insertBefore(newTag, list.childNodes[where]);
 }
-// End
-
-
+//End
 
 /**
- * Give specific onclick functions to each created button
- */
-restaurantInfo.then(
-  info => {
-    for (var i = 0; i < info.length; i++){
-      var targetButton = document.getElementById('restaurantButton'+i);
-      const name = info[i].name;
-      const type = info[i].type;
-      const dining = info[i].dining;
-      const description = info[i].description;
-      targetButton.addEventListener("click", () => {
-        writeOnWebPage('h1', 'Name: ', name, 'details', 0);
-        writeOnWebPage('span', 'Cuisine ', type, 'details', 1);
-        writeOnWebPage('span', 'Dining ', dining, 'details', 2);
-        writeOnWebPage('p', 'Description ', description, 'details', 3);
-      });
-    }
-  }
-)
-// End
-
-
-
-/** 
- * Display restaurants with patio.
- */
-restaurantInfo.then(
-  info => {
-    for (var i = 0; i < info.length; i++){
-      if (info[i].patio === true){
-        var newTag = document.createElement("button");
-        var textnode = document.createTextNode(info[i].name + ' ')
-        newTag.appendChild(textnode);
-        newTag.setAttribute('id','restaurantButton'+i)
-        var list = document.getElementById("patio");
-        list.insertBefore(newTag, list.childNodes[0]);
-      }
-    }
-  }
-)
-// End
-
-
-
-/** 
  * When a restaurant is clicked, the restaurant info
  * should show in the restaurant detail section.
  */
+restaurantInfo.then(info => {
+  for (var i = 0; i < info.length; i++) {
+    var targetButton = document.getElementById("restaurantButton" + i);
+    const name = info[i].name;
+    const type = info[i].type;
+    const dining = info[i].dining;
+    const description = info[i].description;
+    targetButton.addEventListener("click", () => {
+      document.getElementById('details').innerHTML='';
+      writeOnWebPage("h1", "Name: ", name, "details", 0);
+      writeOnWebPage("span", "Cuisine: ", type, "details", 1);
+      writeOnWebPage("span", "Dining: ", dining, "details", 2);
+      writeOnWebPage("p", "Description: ", description, "details", 3);
+    });
+  }
+});
+// End
 
+/**
+ * Display restaurants with patio.
+ */
+restaurantInfo.then(info => {
+  for (var i = 0; i < info.length; i++) {
+    if (info[i].patio === true) {
+      var newTag = document.createElement("button");
+      var textnode = document.createTextNode(info[i].name + " ");
+      newTag.appendChild(textnode);
+      newTag.setAttribute("id", "restaurantButton" + i);
+      var list = document.getElementById("patio");
+      list.insertBefore(newTag, list.childNodes[0]);
+    }
+  }
+});
+// End
 
-
-
-/** 
+/**
  * The user review should be added to the cms.
  */
 
-
-
-/** A function to rank all restaurants based on average score.
+/** 
+ * A function to rank all restaurants based on average score.
  * 
  * Need: a. array.sort()
  *       b. display the sorted version of the array
@@ -131,3 +121,82 @@ restaurantInfo.then(
                 return a.age-b.age
             })
  */
+
+//////////////////
+// JordanH Code //
+//////////////////
+
+//Total Restaurants
+getTotalRestaurants = async () => {
+  const response = await fetch(
+    "http://red-strapi-postgres-heroku.herokuapp.com/Restaurants"
+  );
+  const restaurants = await response.json();
+  return restaurants.length;
+};
+getTotalRestaurants();
+
+const totalRestaurants = getTotalRestaurants();
+totalRestaurants.then(a => {
+  var newTag = document.createElement("span");
+  var textnode = document.createTextNode(a);
+  newTag.appendChild(textnode);
+  let target = document.getElementById("totalRestaurants");
+  target.insertBefore(newTag, target.childNodes[0]);
+});
+
+//Total Reviews
+getTotalReviews = async () => {
+  const response = await fetch(
+    "http://red-strapi-postgres-heroku.herokuapp.com/Reviews"
+  );
+  const reviews = await response.json();
+  return reviews.length;
+};
+getTotalReviews();
+
+const totalReviews = getTotalReviews();
+totalReviews.then(a => {
+  var newTag = document.createElement("span");
+  var textnode = document.createTextNode(a);
+  newTag.appendChild(textnode);
+  let target = document.getElementById("totalReviews");
+  target.insertBefore(newTag, target.childNodes[0]);
+});
+
+//Array of Id's
+restaurantIds = async () => {
+  const response = await fetch(
+    "http://red-strapi-postgres-heroku.herokuapp.com/Reviews"
+  );
+  const restaurants = await response.json();
+  for (let i = 0; i < restaurants.length; i++) {
+    let idArray = restaurants[i].idrestaurant;
+    return idArray;
+  }
+};
+restaurantIds();
+
+//Array of Ratings
+restaurantRatings = async () => {
+  const response = await fetch(
+    "http://red-strapi-postgres-heroku.herokuapp.com/Reviews"
+  );
+  const restaurants = await response.json();
+  for (let i = 0; i < restaurants.length; i++) {
+    let ratingArray = restaurants[i].rating;
+    return ratingArray;
+  }
+};
+restaurantRatings();
+
+//Associate Ratings to Id's
+// rankRestaurants = async () => {
+//   const response = await fetch("http://red-strapi-postgres-heroku.herokuapp.com/Reviews");
+//   const rankingInfo = await response.json();
+//   for (var i = 0; i < rankingInfo.length; i++){
+//     const ratingArray = rankingInfo[i].rating
+//     // rankingInfo.filter(x => x.rating === '5').map(x => x.idrestaurant);
+//   }
+// }
+// console.log(rankRestaurants());
